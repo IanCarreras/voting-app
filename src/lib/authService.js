1a2fd5d304bd1936db7983ab8b01829c54b20da7
Ian <mus5002@aol.com>
2018-03-25 10:29:57 -0400

    lesson changes

--- src/lib/authService.js
+++ src/lib/authService.js
@@ -5,7 +5,7 @@ export default class AuthService {
   constructor(clientId, domain) {
     this.lock = new Auth0Lock(clientId, domain, {
       auth: {
-        redirectUrl: 'http://localhost:3030',
+        redirectUrl: 'http://localhost:3000',
         responseType: 'token'
       }
     })
