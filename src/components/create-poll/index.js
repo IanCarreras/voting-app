b7331a4d461b8ae83deeb72fcec713248749c91d
Ian <mus5002@aol.com>
2018-07-06 07:25:20 -0400

    can add new input fields in the create poll component now

--- src/components/create-poll/index.js
+++ src/components/create-poll/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import AnswerInput from '../answer-input'
 import './index.css'
 
 class CreatePoll extends Component {
@@ -7,10 +8,12 @@ class CreatePoll extends Component {
     super(props);
     this.state = {
       question: '',
-      answers: []
+      answers: [],
+      inputFields: []
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.addInputField = this.addInputField.bind(this)
   }
 
   handleChange(e) {
@@ -20,6 +23,11 @@ class CreatePoll extends Component {
   handleSubmit() {
     const { question, answers } = this.state
     this.props.actions.addImage(question, answers)
+  }
+
+  addInputField() {
+    const updatedArray = this.state.inputFields.concat(<AnswerInput />)
+    this.setState({ inputFields: updatedArray })
   }
 
   render() {
@@ -36,14 +44,30 @@ class CreatePoll extends Component {
             placeholder="enter question here"
           />
 
-          <div className="element-container"><h3 className="answer-label">Answer</h3><button className="add-answer-button">+</button></div>
-          <input
-            className="answer-input"
-            name="answer"
-            onChange={this.handleChange}
-            value={this.state.answers}
-            placeholder="enter answer here"
-          />
+          <div className="element-container">
+            <h3 className="answer-label">Answer</h3>
+            <button className="add-answer-button" onClick={this.addInputField}>+</button>
+          </div>
+
+          <div className="ipnut-field-container">
+            <input
+              className="answer-input"
+              name="answer"
+              onChange={this.handleChange}
+              value={this.state.answers}
+              placeholder="enter answer here"
+            />
+
+            <input
+              className="answer-input"
+              name="answer"
+              onChange={this.handleChange}
+              value={this.state.answers}
+              placeholder="enter answer here"
+            />
+            <AnswerInput />
+            {this.state.inputFields}
+          </div>
 
           <button className="submit-new-poll-button" onClick={this.handleSubmit}>Submit</button>
           <button className="close-window-button" onClick={this.props.actions.createPoll}>Close Window</button>
