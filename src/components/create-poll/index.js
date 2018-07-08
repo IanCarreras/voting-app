3d0b9e74024b0271d66fb93297100519ca2fd759
Ian <mus5002@aol.com>
2018-07-08 11:03:16 -0400

    midified answer input for creating a new poll

--- src/components/create-poll/index.js
+++ src/components/create-poll/index.js
@@ -8,12 +8,10 @@ class CreatePoll extends Component {
     super(props);
     this.state = {
       question: '',
-      answers: [],
-      inputFields: []
+      answers: ''
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.addInputField = this.addInputField.bind(this)
   }
 
   handleChange(e) {
@@ -22,19 +20,15 @@ class CreatePoll extends Component {
 
   handleSubmit() {
     const { question, answers } = this.state
-    this.props.actions.addImage(question, answers)
-  }
-
-  addInputField() {
-    const updatedArray = this.state.inputFields.concat(<AnswerInput />)
-    this.setState({ inputFields: updatedArray })
+    const arrayAnswers = answers.split(',').map(answer => answer.trim())
+    console.log('our data sent to action creator', { question, arrayAnswers })
+    //this.props.actions.addPoll(question, arrayAnswers)
   }
 
   render() {
     return (
       <div className="popup-outer">
         <div className="popup-inner">
-
           <h3>Question</h3>
           <input
             className="question-input"
@@ -43,30 +37,19 @@ class CreatePoll extends Component {
             value={this.state.question}
             placeholder="enter question here"
           />
-
           <div className="element-container">
             <h3 className="answer-label">Answer</h3>
-            <button className="add-answer-button" onClick={this.addInputField}>+</button>
           </div>
 
           <div className="input-field-container">
             <input
               className="answer-input"
-              name="answer"
+              name="answers"
+              placeholder="enter answer here"
               onChange={this.handleChange}
               value={this.state.answers}
-              placeholder="enter answer here"
             />
-
-            <input
-              className="answer-input"
-              name="answer"
-              onChange={this.handleChange}
-              value={this.state.answers}
-              placeholder="enter answer here"
-            />
-            <AnswerInput />
-            {this.state.inputFields}
+            <div>enter answer options seperated by comma</div>
           </div>
 
           <button className="submit-new-poll-button" onClick={this.handleSubmit}>Submit</button>
